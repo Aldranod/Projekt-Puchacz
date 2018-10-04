3e0b4dd09b9138e586a793cd8f8b8b226e28e0c2
Sotha <minesotha@gmail.com>
2018-10-04 20:02:50 +0200

    sowa po klięciu skacze smutno w dół (#2)

---
+++ Assets/Scripts/owlMove.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class owlMove : MonoBehaviour {
+
+
+    bool moving = false;
+    float t;
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+
+        if (moving)
+        {
+            // when the cube has moved over 1 second report it's position
+            //t = t + Time.deltaTime;
+            //if (t > 1.0f)
+            //{
+            //    Debug.Log(gameObject.transform.position.y + " : " + t);
+            //    t = 0.0f;
+            //    moving = false;
+            //}
+        }
+
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("owl click");
+        Move();
+    }
+
+    void Move()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector3(-2, 10, 0);
+        moving = true;
+
+    }
+
+
+}
