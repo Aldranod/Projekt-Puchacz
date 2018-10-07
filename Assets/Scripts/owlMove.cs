ca23874b4482204b3a57d2e8cf9d31035358c162
Sotha <minesotha@gmail.com>
2018-10-07 21:35:25 +0200

    dodanie labelki pokazującej moc skoku (w procentach) (#2)

--- Assets/Scripts/owlMove.cs
+++ Assets/Scripts/owlMove.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class owlMove : MonoBehaviour {
 
     //ile skoków może zrobić sowa
     public int PossibleJumpsCount = 1;
+    public Text PowerLevelText;
 
     bool addVelovity = false;
     float time;
@@ -17,7 +19,7 @@ public class owlMove : MonoBehaviour {
 
     void Start()
     {
-
+        PowerLevelText.enabled = false;
     }
 
     void Update()
@@ -27,6 +29,12 @@ public class owlMove : MonoBehaviour {
         {
             time += Time.deltaTime;
             jumpVelocity += new Vector3(-(Time.deltaTime*10), Time.deltaTime*10.0f, 0);
+            //TODO: dodać jakiś dzielnik, żeby liczba procentów nie zapierdalała
+            //if (time % 5 == 0.0f)
+            //{
+                PowerLevelText.text = (time * 100).ToString("0.00") + "%";
+            //}
+
             //dokonaj skoku po określonym czasie, nie pozwól trzymać w nieskończoność
             if (time > 1.0f)
             {
@@ -38,6 +46,7 @@ public class owlMove : MonoBehaviour {
 
     void OnMouseDown()
     {
+        PowerLevelText.enabled = true;
         if (jumpCount < PossibleJumpsCount)
         {
             time = 0.0f;
@@ -61,6 +70,7 @@ public class owlMove : MonoBehaviour {
             GetComponent<Rigidbody2D>().velocity = jumpVelocity;
             addVelovity = false;
             jumpCount++;
+            PowerLevelText.enabled = false;
         }
     }
 
