cc570bc3ed506475c49ab69bb9b56214fd9dcd7f
Sotha <minesotha@gmail.com>
2018-10-10 20:45:28 +0200

    obsługa kolizji sowy (#11)

--- Assets/Scripts/owlMove.cs
+++ Assets/Scripts/owlMove.cs
@@ -10,39 +10,24 @@ public class owlMove : MonoBehaviour {
     public Text PowerLevelText;
     //linia która pokazuje jak będzie wystrzeliwana sowa
     public GameObject JumpLine;
+    public Transform startPoint;
 
     bool addVelovity = false;
     float time;
     //ile skoków sowa wykonała
     int jumpCount = 0;
-    
+
+
+    #region eventy
     void Start()
     {
         PowerLevelText.enabled = false;
         JumpLine.GetComponent<LineRenderer>().SetPosition(0, transform.position);
     }
 
-    void Update()
+    void FixedUpdate()
     {
-
-        if (addVelovity)
-        {
-
-            UpdateLine(Camera.main.ScreenToWorldPoint(Input.mousePosition));
-            time += Time.deltaTime;
-           // jumpVelocity += new Vector3(Time.deltaTime, Time.deltaTime, 0);
-            //TODO: dodać jakiś dzielnik, żeby liczba procentów nie zapierdalała
-            //if (time % 5 == 0.0f)
-            //{
-                PowerLevelText.text = (time * 100).ToString("0.00") + "%";
-            //}
-
-            //dokonaj skoku po określonym czasie, nie pozwól trzymać w nieskończoność
-            if (time > 1.0f)
-            {
-                Jump();
-            }
-       }
+        CheckJumping();
 
     }
 
@@ -58,7 +43,6 @@ public class owlMove : MonoBehaviour {
         }
     }
 
-
     void OnMouseUp()
     {
         Jump();
@@ -70,11 +54,70 @@ public class owlMove : MonoBehaviour {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
         finalVector.x = transform.position.x - mousePos.x;
-        finalVector.y  = transform.position.y - mousePos.y;
+        finalVector.y = transform.position.y - mousePos.y;
 
         return finalVector;
     }
 
+    void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("OnCollisionEnter2D");
+        if (col.gameObject.tag == "floor")
+        {
+            Debug.Log("floorhit");
+        }
+        else if (col.gameObject.tag == "victim")
+        {
+            Debug.Log("kill!");
+        }
+    }
+    #endregion
+
+    #region obsługa kolizji
+
+    /// <summary>
+    /// po uderzeniu odlicza chwile, zanim zrestartuje pozycje sowy
+    /// </summary>
+    void StartCountToReset()
+    {
+
+    }
+
+    //resetuje pozycje sowy
+    void Reset()
+    {
+        jumpCount = 0;
+        transform.position = startPoint.position;
+    }
+    #endregion
+
+
+    #region obsługa skoku
+    /// <summary>
+    /// Sprawdzanie, czy sytępuje skok i jego obsługa
+    /// </summary>
+    void CheckJumping()
+    {
+        if (addVelovity)
+        {
+
+            UpdateLine(Camera.main.ScreenToWorldPoint(Input.mousePosition));
+            time += Time.deltaTime;
+            // jumpVelocity += new Vector3(Time.deltaTime, Time.deltaTime, 0);
+            //TODO: dodać jakiś dzielnik, żeby liczba procentów nie zapierdalała
+            //if (time % 5 == 0.0f)
+            //{
+            PowerLevelText.text = (time * 100).ToString("0.00") + "%";
+            //}
+
+            //dokonaj skoku po określonym czasie, nie pozwól trzymać w nieskończoność
+            if (time > 1.0f)
+            {
+                Jump();
+            }
+        }
+
+    }
 
     void Jump()
     {
@@ -99,5 +142,5 @@ public class owlMove : MonoBehaviour {
         JumpLine.SetActive(false);
     }
 
-
+    #endregion
 }
