f3715fff2210cfaa154ecc40a3f9e3406a518193
Aldranod <kurzydlo.b@gmail.com>
2018-10-14 20:00:55 +0200

    mysz sie obraca (closes #12)

--- Assets/Scripts/Game/EnemyPathing.cs
+++ Assets/Scripts/Game/EnemyPathing.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,23 +7,32 @@ public class EnemyPathing : MonoBehaviour {
     public List<Transform> waypoints;
     public float moveSpeed = 2f;
     int waypointIndex = 0;
+    float previousXPos;
 
 	// Use this for initialization
 	void Start () {
         transform.position = waypoints[waypointIndex].transform.position;
+        previousXPos = transform.position.x;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-        //if transform.position.x < previous.transform.position.x
-        //{
-        //    mousego left
-        //}
-        //else
-        //{
-        //    mousego right
-        //}
+	void Update ()
+    {
+        previousXPos = transform.position.x;
         Move();
+        MouseRotate();
+    }
+
+    private void MouseRotate()
+    {
+        if (transform.position.x > previousXPos)
+        {
+            transform.rotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            transform.rotation = Quaternion.Euler(0, 0, 0);
+        }
     }
 
     private void Move()
@@ -32,7 +42,6 @@ public class EnemyPathing : MonoBehaviour {
             var targetPosition = waypoints[waypointIndex].transform.position;
             var movementThisFrame = moveSpeed * Time.deltaTime;
             transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
-
             if(transform.position == targetPosition)
             {
                 waypointIndex++;
